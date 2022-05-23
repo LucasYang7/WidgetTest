520843bc7139b2edd7b1d5664d51a13e871de3bb
yangzhe09 <yangzhe09@baidu.com>
2022-05-23 21:04:48 +0800

    add AdapterViewFlipper

--- app/src/main/java/com/xiaozhezhe/widgettest/MainActivity.kt
+++ app/src/main/java/com/xiaozhezhe/widgettest/MainActivity.kt
@@ -2,19 +2,33 @@ package com.xiaozhezhe.widgettest
 
 import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.tv_add_app_widget).setOnClickListener {
+        tv_add_app_widget.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 AppWidgetManager.getInstance().addAppWidgetInApp(
                     this@MainActivity.applicationContext, false
                 )
             }
         }
+        val numberImages = intArrayOf(
+            R.drawable.one,
+            R.drawable.two,
+            R.drawable.three,
+            R.drawable.four,
+            R.drawable.five,
+            R.drawable.six
+        )
+        val adapter = ViewFlipperAdapter(this@MainActivity, numberImages)
+        viewFlipper.flipInterval = 42
+        viewFlipper.adapter = adapter
+        viewFlipper.isAutoStart = true
+        viewFlipper.startFlipping()
     }
 }
---
+++ app/src/main/java/com/xiaozhezhe/widgettest/ViewFlipperAdapter.kt
@@ -0,0 +1,50 @@
+package com.xiaozhezhe.widgettest
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
+
+class ViewFlipperAdapter : BaseAdapter {
+    private var mContext: Context? = null
+    private var imageList: IntArray? = null
+    var inflter: LayoutInflater? = null
+
+    constructor(
+        applicationContext: Context,
+        colorImages: IntArray
+    ) {
+        mContext = applicationContext
+        imageList = colorImages
+        inflter = LayoutInflater.from(applicationContext)
+    }
+
+    override fun getCount(): Int {
+        return imageList?.size ?: 0
+    }
+
+    override fun getItem(position: Int): Any? {
+        return null
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val view = inflter?.inflate(R.layout.item_widget_image, null)
+        val imageView = view?.findViewById<ImageView>(R.id.widgetImage)
+        mContext?.let {
+            Glide.with(it)
+                .load(imageList?.get(position))
+                .into(imageView!!)
+        }
+        return view
+    }
+}
